568aa9cbecc7eb42a3eb33feed03dd27785637bb
rahulgidde <rahulgidde1212@gmail.com>
2020-03-03 15:05:28 +0530

    ADDED:Check Winner

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -4,6 +4,10 @@ echo "-------------Welcome To Tic Tac Toe Game-----------"
 #CONSTANT
 ARRAYSIZE=8
 
+#VARIABLE
+winner=0
+position=0
+
 #DECLARE ARRAY
 declare -a board
 
@@ -51,7 +55,145 @@ function toss()
    echo $win
 }
 
-assignLetter
-toss
-displayBoard
 
+#FUNCTION TO CHECK WINNER HORIZONTALY
+function checkHorizontal()
+{
+   local row=3
+   local value=$1
+   for (( index1=0; index1<$row; index1++ ))
+   do
+      local flag=0
+      for (( index2=$index1*$row; index2<=$(($(($index1*$row))+2)); index2++ ))
+      do
+         if [ ${board[$index2]} == $value ]
+         then
+            ((flag++))
+         fi
+      done
+      if [ $flag -eq $row ]
+      then
+         returnValue=1
+         break;
+      else
+         returnValue=0
+      fi
+   done
+   echo $returnValue
+}
+
+#FUNCTION TO CHECK WINNER VERTICALY
+function checkVertical()
+{
+   local row=3
+   local value=$1
+   for (( index1=0; index1<$row; index1++ ))
+   do
+      local flag=0
+      for (( index2=$index1; index2<=$index1+6; index2=$index2+3 ))
+      do
+         if [ ${board[$index2]} == $value ]
+         then
+            ((flag++))
+         fi
+      done
+      if [ $flag -eq $row ]
+      then
+         returnValue=1
+         break;
+      else
+         returnValue=0
+      fi
+   done
+   echo $returnValue
+}
+
+#FUNCTION TO CHECK WINNER DIGONALY
+function checkDiagonal()
+{
+   if [[ ${board[0]} == ${board[4]} ]] && [[ ${board[4]}  ==  ${board[8]} ]] && [[ ${board[8]} == $1 ]]
+   then
+      echo 1
+   elif [[ ${board[2]} == ${board[4]} ]] && [[ ${board[4]}  ==  ${board[6]} ]] && [[ ${board[6]} == $1 ]]
+   then
+      echo  1
+   else
+      echo 0
+   fi
+}
+
+#FUNCTION TO CHECK WINNER
+function getBoard()
+{
+	value=$1
+	board[$position]=$value
+	displayBoard
+
+	if [[ 1 == $(checkHorizontal $value) ]]
+	then
+		winner=1
+	elif [[ 1 == $(checkVertical $value) ]]
+	then
+		winner=1
+	elif [[ 1 == $(checkDiagonal $value) ]]
+	then
+		winner=1
+	else
+		winner=0
+	fi
+}
+
+#FUNCTION FOR PLAYER TURN
+function playerTurn()
+{
+	read -p "Enter your position in between 0 to 8: " position
+	if [ $position -gt 8 ]
+	then
+		echo "Enter Valid Position"
+	elif [ ${board[$position]} == x ] || [ ${board[$position]} == o ]
+	then
+		echo "Position Occupied"
+	else
+		getBoard $player $position
+		if [[ $winner == 1 ]]
+		then
+			echo "You Win"
+		fi
+		flag=1
+	fi
+}
+
+#FUNCTION FOR PLAYER TURN
+function computerTurn()
+{
+	read -p "Enter your position in between 0 to 8: " position
+	if [ $position -gt 8 ]
+	then
+		echo "Enter Valid Position"
+	elif [ ${board[$position]} == x ] || [ ${board[$position]} == o ]
+	then
+		echo "Position Occupied"
+	else
+		getBoard $computer $position
+		flag=0
+	fi
+	if [[ $winner == 1 ]]
+	then
+		echo "Computer Win"
+	fi
+}
+
+assignLetter
+flag=$(toss)
+displayBoard
+while [ $winner -ne 1 ]
+do
+	if [ $flag -eq 0 ]
+	then
+		echo "Your Turn:"
+		playerTurn $player
+	else
+		echo "Computer Turn:"
+		computerTurn $computer
+	fi
+done
