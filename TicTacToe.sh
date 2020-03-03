f8eb84742957ce8950c8ab821271882d40f5024f
rahulgidde <rahulgidde1212@gmail.com>
2020-03-03 09:17:55 +0530

    ADDED:Reset Board Functon

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,25 @@
 #!/bin/bash -x
 echo "-------------Welcome To Tic Tac Toe Game-----------"
+
+#CONSTANT
+ARRAYSIZE=8
+
+#DECLARE ARRAY
+declare -a board
+
+#FUNCTION TO RESET THE BOARD
+for (( index=0; index<=$ARRAYSIZE; index++ ))
+do
+   board[$index]="-"
+done
+
+#FUNCTION TO PRINT BOARD
+function printBoard()
+{
+   echo " | "${board[0]}" | "${board[1]}" | "${board[2]}" | "
+   echo " -------------"
+   echo " | "${board[3]}" | "${board[4]}" | "${board[5]}" | "
+   echo " -------------"
+   echo " | "${board[6]}" | "${board[7]}" | "${board[8]}" | "
+   echo " -------------"
+}
