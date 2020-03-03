56e55eaade1731cab57549fdfdbd89db9c11d548
rahulgidde <rahulgidde1212@gmail.com>
2020-03-03 17:12:06 +0530

    MODIFIED:Indentation

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -16,111 +16,111 @@ declare -a board
 #FUNCTION TO RESET THE BOARD
 for (( index=0; index<=$ARRAYSIZE; index++ ))
 do
-   board[$index]="-"
+	board[$index]="-"
 done
 
 #FUNCTION TO PRINT BOARD
 function displayBoard()
 {
-   echo " | "${board[0]}" | "${board[1]}" | "${board[2]}" | "
-   echo " -------------"
-   echo " | "${board[3]}" | "${board[4]}" | "${board[5]}" | "
-   echo " -------------"
-   echo " | "${board[6]}" | "${board[7]}" | "${board[8]}" | "
-   echo " -------------"
+	echo " | "${board[0]}" | "${board[1]}" | "${board[2]}" | "
+	echo " -------------"
+	echo " | "${board[3]}" | "${board[4]}" | "${board[5]}" | "
+	echo " -------------"
+	echo " | "${board[6]}" | "${board[7]}" | "${board[8]}" | "
+	echo " -------------"
 }
 
 #FUNCTION TO ASSIGN LETTER TO PLAYER
 function assignLetter()
 {
-   check=$((RANDOM%2))
-   if [ $check -eq 0 ]
-   then
-      player="o"
-      computer="x"
-   else
-      player="x"
-      computer="o"
-   fi
+	check=$((RANDOM%2))
+	if [ $check -eq 0 ]
+	then
+		player="o"
+		computer="x"
+	else
+		player="x"
+		computer="o"
+	fi
 }
 
 #FUNCTION TO TOSS FOR WHO WILL PLAY FIRST
 function toss()
 {
-   randomToss=$((RANDOM%2))
-   if [ $randomToss -eq 0 ]
-   then
-      win=0
-   else
-      win=1
-   fi
-   echo $win
+	randomToss=$((RANDOM%2))
+	if [ $randomToss -eq 0 ]
+	then
+		win=0
+	else
+		win=1
+	fi
+	echo $win
 }
 
 #FUNCTION TO CHECK WINNER HORIZONTALY
 function checkHorizontal()
 {
-   local row=3
-   local value=$1
-   for (( index1=0; index1<$row; index1++ ))
-   do
-      local flag=0
-      for (( index2=$index1*$row; index2<=$(($(($index1*$row))+2)); index2++ ))
-      do
-         if [ ${board[$index2]} == $value ]
-         then
-            ((flag++))
-         fi
-      done
-      if [ $flag -eq $row ]
-      then
-         returnValue=1
-         break;
-      else
-         returnValue=0
-      fi
-   done
-   echo $returnValue
+	local row=3
+	local value=$1
+	for (( index1=0; index1<$row; index1++ ))
+	do
+		local flag=0
+		for (( index2=$index1*$row; index2<=$(($(($index1*$row))+2)); index2++ ))
+		do
+			if [ ${board[$index2]} == $value ]
+			then
+				((flag++))
+				fi
+		done
+		if [ $flag -eq $row ]
+		then
+			returnValue=1
+			break;
+		else
+			returnValue=0
+		fi
+	done
+	echo $returnValue
 }
 
 #FUNCTION TO CHECK WINNER VERTICALY
 function checkVertical()
 {
-   local row=3
-   local value=$1
-   for (( index1=0; index1<$row; index1++ ))
-   do
-      local flag=0
-      for (( index2=$index1; index2<=$index1+6; index2=$index2+3 ))
-      do
-         if [ ${board[$index2]} == $value ]
-         then
-            ((flag++))
-         fi
-      done
-      if [ $flag -eq $row ]
-      then
-         returnValue=1
-         break;
-      else
-         returnValue=0
-      fi
-   done
-   echo $returnValue
+	local row=3
+	local value=$1
+	for (( index1=0; index1<$row; index1++ ))
+	do
+		local flag=0
+		for (( index2=$index1; index2<=$index1+6; index2=$index2+3 ))
+		do
+			if [ ${board[$index2]} == $value ]
+			then
+				((flag++))
+			fi
+		done
+		if [ $flag -eq $row ]
+		then
+			returnValue=1
+			break;
+		else
+			returnValue=0
+		fi
+	done
+	echo $returnValue
 }
 
 #FUNCTION TO CHECK WINNER DIGONALY
 function checkDiagonal()
 {
-   if [[ ${board[0]} == ${board[4]} ]] && [[ ${board[4]}  ==  ${board[8]} ]] && [[ ${board[8]} == $1 ]]
-   then
-      echo 1
-   elif [[ ${board[2]} == ${board[4]} ]] && [[ ${board[4]}  ==  ${board[6]} ]] && [[ ${board[6]} == $1 ]]
-   then
-      echo  1
+	if [[ ${board[0]} == ${board[4]} ]] && [[ ${board[4]}  ==  ${board[8]} ]] && [[ ${board[8]} == $1 ]]
+	then
+		echo 1
+	elif [[ ${board[2]} == ${board[4]} ]] && [[ ${board[4]}  ==  ${board[6]} ]] && [[ ${board[6]} == $1 ]]
+	then
+		echo  1
    else
       echo 0
-   fi
+	fi
 }
 
 #FUNCTION TO CHECK WINNING MOVE FOR COMPUTER
