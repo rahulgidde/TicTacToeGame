b8f9091fabb7969e489c02098609b12f34bc2c66
rahulgidde <rahulgidde1212@gmail.com>
2020-03-03 16:18:05 +0530

    ADDED:Check Winning Move Function

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -7,6 +7,7 @@ ARRAYSIZE=8
 #VARIABLE
 winner=0
 position=0
+turnCount=0
 
 #DECLARE ARRAY
 declare -a board
@@ -121,25 +122,107 @@ function checkDiagonal()
    fi
 }
 
-#FUNCTION TO CHECK WINNER
-function getBoard()
+#FUNCTION TO CHECK WINNING MOVE FOR COMPUTER
+function checkWinningMove()
 {
-	value=$1
-	board[$position]=$value
-	displayBoard
-	if [[ 1 == $(checkHorizontal $value) ]]
+	#CHECK ROW WISE
+	local row=0
+	local count1=1
+	local count2=2
+	local checkSign=$1
+	played=0
+	for (( index=1; index<=3; index++ ))
+	do
+		if [[ ${board[$row]} == $checkSign ]] && [[ ${board[$row]} == ${board[$count1]} ]] && [[  ${board[$(($count1+1))]} == "-" ]] || [[ ${board[$count1]} == $checkSign ]] && [[ ${board[$count1]} == ${board[$row]} ]] && [[ ${board[$(($count1+1))]} == "-" ]]
+		then
+			board[$(($count1+1))]=$computer
+			played=1
+			return
+		elif [[ ${board[$row]} == $checkSign ]] && [[ ${board[$row]} == ${board[$count2]} ]] && [[ ${board[$(($count2-1))]} == "-" ]] || [[ ${board[$count2]} == $checkSign ]] && [[ ${board[$count2]} == ${board[$row]} ]] && [[ ${board[$(($count2-1))]} == "-" ]]
+		then
+			board[$(($count2-1))]=$computer
+			played=1
+			return
+		elif [[ ${board[$count1]}${board[$count2]} == $checkSign$checkSign  &&  ${board[$(($count1-1))]} == "-" ]]
+		then
+			board[$(($count1-1))]=$computer
+			played=1
+			return
+		else
+			count1=$(($count1+3))
+			count2=$(($count2+3))
+			row=$(($row+3))
+		fi
+	done
+
+	#CHECK COLUMN WISE
+	column=0
+	count1=3
+	count2=6
+	for (( index=1; index<=3; index++ ))
+	do
+		if [[ ${board[$column]} == $checkSign ]] && [[ ${board[$column]} == ${board[$count1]} ]] && [[ ${board[$(($count1+3))]} == "-" ]] || [[ ${board[$count1]} == $checkSign ]] && [[ ${board[$count1]} == ${board[$column]} ]] && [[ ${board[$(($count1+3))]} == "-" ]]
+		then
+			board[$(($count1+3))]=$computer
+			played=1
+			return
+		elif [[ ${board[$column]}  == $checkSign  ]] && [[ ${board[$column]} == ${board[$count2]} ]] && [[ ${board[$(($count2-3))]} == "-" ]] || [[ ${board[$count2]} == $checkSign ]] && [[ ${board[$count2]} == ${board[$column]} ]] && [[ ${board[$(($count2-3))]} == "-" ]]
+		then
+			board[$(($count2-3))]=$computer
+			played=1
+			return
+		elif [[ ${board[$count1]}${board[$count2]} == $checkSign$checkSign  &&  ${board[$(($count1-3))]} == "-" ]]
+		then
+			board[$(($count1-3))]=$computer
+			played=1
+			return
+		else
+			count1=$(($count1+1))
+			count2=$(($count2+1))
+			column=$(($column+1))
+		fi
+	done
+
+	#CHECK DIAGONAL
+	diagonal=0
+	count1=4
+	count2=8
+	if [[ ${board[$diagonal]} == $checkSign ]] && [[ ${board[$diagonal]} == ${board[$count1]} ]] && [[ ${board[$(($count1+4))]} == "-" ]] || [[ ${board[$count1]} == $checkSign ]] && [[ ${board[$count1]} == ${board[$diagonal]} ]] && [[ ${board[$(($count1+4))]} == "-" ]]
 	then
-		winner=1
-	elif [[ 1 == $(checkVertical $value) ]]
+		board[$(($count1+4))]=$computer
+		played=1
+		return
+	elif [[ ${board[$diagonal]} == $checkSign  ]]  && [[  ${board[$diagonal]} == ${board[$count2]} ]] && [[ ${board[$(($count2-4))]} == "-" ]] || [[ ${board[$count2]} == $checkSign ]] && [[ ${board[$count2]} == ${board[$diagonal]} ]] && [[ ${board[$(($count2-4))]} == "-" ]]
 	then
-		winner=1
-	elif [[ 1 == $(checkDiagonal $value) ]]
+		board[$(($count2-4))]=$computer
+		played=1
+		return
+	elif  [[ ${board[$count1]}${board[$count2]} == $checkSign$checkSign  &&  ${board[$(($count1-4))]} == "-" ]]
 	then
-		winner=1
-	else
-		winner=0
+		board[$(($count1-4))]=$computer
+		played=1
+		return
+	fi
+	count2=$(($count2-2))
+	diagonal=$(($diagonal+2))
+	if [[ ${board[$diagonal]} == $checkSign ]] && [[ ${board[$diagonal]} == ${board[$count1]} ]] && [[ ${board[$(($count1+2))]} == "-" ]] || [[ ${board[$count1]} == $checkSign ]] && [[ ${board[$count1]} == ${board[$diagonal]} ]] && [[ ${board[$(($count1+2))]} == "-" ]]
+	then
+		board[$(($count1+2))]=$computer
+		played=1
+		return
+	elif [[ ${board[$diagonal]} == $checkSign  ]]  && [[  ${board[$diagonal]} == ${board[$count2]} ]] && [[ ${board[$(($count2-2))]} == "-" ]] || [[ ${board[$count2]} == $checkSign ]] && [[ ${board[$count2]} == ${board[$diagonal]} ]] && [[ ${board[$(($count2-2))]} == "-" ]]
+	then
+		board[$(($count2-2))]=$computer
+		played=1
+		return
+	elif  [[ ${board[$count1]}${board[$count2]} == $checkSign$checkSign  &&  ${board[$(($count1-4))]} == "-" ]]
+	then
+		board[$(($count1-4))]=$computer
+		played=1
+		return
 	fi
 }
+
 
 #FUNCTION FOR PLAYER TURN
 function playerTurn()
@@ -152,33 +235,61 @@ function playerTurn()
 	then
 		echo "Position Occupied"
 	else
-		getBoard $player $position
-		if [[ $winner == 1 ]]
-		then
-			echo "You Win"
-		fi
 		flag=1
+		board[$position]=$player
 	fi
+	displayBoard
+	if [[ 1 == $(checkHorizontal $player) ]]
+	then
+		winner=1
+	elif [[ 1 == $(checkVertical $player) ]]
+	then
+		winner=1
+	elif [[ 1 == $(checkDiagonal $player) ]]
+	then
+		winner=1
+	else
+		winner=0
+	fi
+	if [[ $winner == 1 ]]
+	then
+		echo "You Win"
+	fi
+	((turnCount++))
 }
 
 #FUNCTION FOR COMPUTER TURN
 function computerTurn()
 {
-	position=$((0+RANDOM%8))
-	if [ $position -gt 8 ]
+	checkWinningMove $computer
+	if [[ $played == 0 ]]
 	then
-		echo "Enter Valid Position"
-	elif [ ${board[$position]} == x ] || [ ${board[$position]} == o ]
+		position=$((RANDOM%9))
+		while [[ ${board[$position]} != "-" ]]
+		do
+			position=$((RANDOM%9))
+		done
+		board[$position]=$computer
+	fi
+	displayBoard
+	if [[ 1 == $(checkHorizontal $computer) ]]
 	then
-		echo "Position Occupied"
+		winner=1
+	elif [[ 1 == $(checkVertical $computer) ]]
+	then
+		winner=1
+	elif [[ 1 == $(checkDiagonal $computer) ]]
+	then
+		winner=1
 	else
-		getBoard $computer $position
-		flag=0
+		winner=0
 	fi
 	if [[ $winner == 1 ]]
 	then
 		echo "Computer Win"
 	fi
+	flag=0
+	((turnCount++))
 }
 
 assignLetter
@@ -188,10 +299,13 @@ while [ $winner -ne 1 ]
 do
 	if [ $flag -eq 0 ]
 	then
-		echo "Your Turn:"
 		playerTurn $player
 	else
-		echo "Computer Turn:"
 		computerTurn $computer
 	fi
+	if [[ $turnCount -eq $TOTAL_TURN ]]
+	then
+		echo "It's Tie"
+	fi
 done
+
